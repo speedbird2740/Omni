b241e1f494d210cf50bb2a85c62f39c9ff38e6e5
stalker0000 <79029011+speedbird2740@users.noreply.github.com>
2022-02-10 20:45:04 -0500

    Add code for setup utility

--- setup_cli.py
+++ setup_cli.py
@@ -1,31 +1,63 @@
 import argparse
+import json
+import os
 import platform
+import sys
+import traceback
+
 import pkg_resources
 
-from subprocess import call
 
-pkg_reqs = []
+def install_pkg(package):
+    os.system(f"{sys.executable} -m pip install {package}")
 
 
 def check_reqs():
     warnings = []
     python_version = platform.python_version_tuple()
 
+    print("Checking Python version...")
+
     if int(python_version[0]) < 3:
         raise Exception("Python 3.x required")
     if int(python_version[1]) != 7:
         warnings.append("WARNING: some Python packages may not install/work properly without Python 3.7")
 
+    print("Checking operating system...")
     if not platform.system() == "Windows":
         raise Exception("Omni only supports the Windows operating system")
 
-    packages = [dist.project_name for dist in pkg_resources.working_set]
-    # More code in development
+    print("Cloning repository...")
 
-    return warnings
+    if not os.path.exists("temp/setup_cli"):
+        os.mkdir("temp/setup_cli")
+
+    git.Repo.clone_from("https://github.com/speedbird2740/Omni", "temp/setup_cli/Omni_repo/")
+
+    try:
+        update_reqs = json.load(open("updates.json", "r"))
+    except Exception:
+        traceback.print_exc()
+        print("Failed to load update requirements. Get update requirements (updates.json) from the "
+              "Omni repository and try again")
+
+        input("Press enter to exit: ")
+        sys.exit()
+
+    print("Checking python packages...")
+
+    from temp.setup_cli.Omni_repo.main import version
+
+    pkg_reqs = update_reqs[version]["packages"]
+    packages = [dist.project_name for dist in pkg_resources.working_set]
+
+    for pkg in pkg_reqs:
+        if pkg not in packages:
+            install_pkg(pkg)
 
 
 if __name__ == "__main__":
+    print("Warning: setup utility is a work in progress")
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-S", "--setup", help="Setup the Omni instance")
@@ -33,6 +65,13 @@ if __name__ == "__main__":
     parser.add_argument("-U", "--update", help="Update the Omni instance")
 
     args = parser.parse_args()
+
+    try:
+        import git
+    except ImportError:
+        input("Git is a required package for this utility. Press enter to install it: ")
+        os.system(f"{sys.executable} -m pip install gitpython")
+        import git
 
     if args.setup:
         pass
